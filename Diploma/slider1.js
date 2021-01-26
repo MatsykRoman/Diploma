7005cc116b0c5de7e38b45bee9f760720545f107
MatsykRoman <piupiupiusuka@gmail.com>
2021-01-26 18:00:20 +0200

    My Diploma

---
+++ Diploma/slider1.js
@@ -0,0 +1,13 @@
+new Swiper('.text-slider', {
+  slideToClickedSlide: true,
+  keyboard: {
+    enable: true,
+    onlyInViewport: true,
+    pageUpDown: true
+  },
+  mousewheel: {
+    sensitivity: 1
+  },
+  loop: true,
+  speed: 800,
+});
---
+++ Diploma/slider3.js
@@ -0,0 +1,17 @@
+new Swiper('.third-slider', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  slideToClickedSlide: true,
+  keyboard: {
+    enable: true,
+    onlyInViewport: true,
+    pageUpDown: true
+  },
+  loop: true,
+  speed: 800,
+  mousewheel: {
+    sensitivity: 1
+  },
+});
